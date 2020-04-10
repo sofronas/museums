3bea1fd3e40b53d3f5b4f49d2428d40d3bb71867
sofronas <sofronas.konstantinos@gmail.com>
2020-04-10 22:55:20 +0300

    Commit changes

---
+++ js/changer.js
@@ -0,0 +1,8 @@
+function FirstMuseum() {
+	var museum = document.getElementById('firstm');
+	var text = document.getElementById('lorem');
+
+
+	text.innerHTML = '<div class="container"> <div class="col"> <p> text </p> </div> </div>'
+
+}
