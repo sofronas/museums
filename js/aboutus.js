7db214cff7adb38a8dfac860f7ca20e8610a2e0e
sofronas <sofronas.konstantinos@gmail.com>
2020-04-16 22:37:45 +0300

    Changes

---
+++ js/aboutus.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	
+	$("#firstItemOfAboutUs").click(function(){
+		$("#faq1").toggle();
+	});
+	$("#secondItemOfAboutUs").click(function(){
+		$("#faq2").toggle();
+	});
+	$("#thirdItemOfAboutUs").click(function(){
+		$("#faq3").toggle();
+	});
+});
